e4331c43a51531b8d9cd0712ca39fb3d9793ca9e
Charlycop <charlycop@free.fr>
2019-10-07 13:35:48 +0200

    Utilisation de sort()

--- TME3/src/vector.cpp
+++ TME3/src/vector.cpp
@@ -5,6 +5,7 @@
 #include "timer.h"
 #include <fstream>
 #include <regex>
+#include <algorithm>
 
 using namespace std;
 #include "GPL_2_text.h"
@@ -24,43 +25,18 @@ namespace vector_bench{
             vecteurTexte.push_back(GPL_2_text[n]);
 
             // On trie par ordre alphébétique
-            size_t i=n;
-            while(i > 0){
-                string temp;
-                size_t  j=i;
-
-                // On met en minuscule pour comparer
-                string moti = vecteurTexte[i], motjmoins1 = vecteurTexte[j-1];
-                transform(moti.begin(),moti.end(),moti.begin(),::tolower);
-                transform(motjmoins1.begin(),motjmoins1.end(),motjmoins1.begin(),::tolower);
-
-                while (j>0 && (moti < motjmoins1)) {
-                    --j ; // On compare au moins précédent
-                    if (j>0){
-                        // on met en minuscule le mot précédent
-                        motjmoins1 = vecteurTexte[j-1];
-                        transform(motjmoins1.begin(),motjmoins1.end(),motjmoins1.begin(),::tolower);
-                    }
-                }
-
-                // Si j!=i c'est qu'on a trouvé une meilleurs place
-                if (j != i){
-                    temp            = vecteurTexte[j];
-                    vecteurTexte[j] = vecteurTexte[i];
-                    vecteurTexte[i] = temp;
-                }else --i; // Sinon on passe au mot suivant
-                
-            };
+            sort(vecteurTexte.begin(), vecteurTexte.end());
         }
 
+        // On affiche le nombre d'élements du vecteur
+        cout << "Le vecteur contient " << vecteurTexte.size()<< " elements." << endl;
+        
         // On affiche tous les élements sur une ligne
         //for(string motCourant : vecteurTexte) cout << motCourant << " ";
 
         // On arrête le chrono
         chrono.stop();
         cout << "SortEachInsert() "<< chrono;
-
-
     }
 
     void backInsert    (){
@@ -78,38 +54,12 @@ namespace vector_bench{
         // On affiche le nombre d'élements du vecteur
         cout << "Le vecteur contient " << vecteurTexte.size()<< " elements." << endl;
 
-        // On trie par ordre alphébétique
-        size_t i=1;
-        do{
-            string temp;
-            size_t  j=i;
-
-            // On met en minuscule pour comparer
-            string moti = vecteurTexte[i], motjmoins1 = vecteurTexte[j-1];
-            transform(moti.begin(),moti.end(),moti.begin(),::tolower);
-            transform(motjmoins1.begin(),motjmoins1.end(),motjmoins1.begin(),::tolower);
-
-            while (j>0 && (moti < motjmoins1)) {
-                --j ; // On compare au moins précédent
-                if (j>0){
-                    // on met en minuscule le mot précédent
-                    motjmoins1 = vecteurTexte[j-1];
-                    transform(motjmoins1.begin(),motjmoins1.end(),motjmoins1.begin(),::tolower);
-                }
-            }
-
-            // Si j!=i c'est qu'on a trouvé une meilleurs place
-            if (j != i){
-                temp            = vecteurTexte[j];
-                vecteurTexte[j] = vecteurTexte[i];
-                vecteurTexte[i] = temp;
-            }else ++i; // Sinon on passe au mot suivant
-            
-        }while(i < vecteurTexte.size());
+        // On trie par ordre alphabétique
+        sort(vecteurTexte.begin(), vecteurTexte.end());
 
         // On affiche tous les élements sur une ligne
         //for(string motCourant : vecteurTexte) cout << motCourant << " ";
-
+    
         // On arrête le chrono
         chrono.stop();
         cout << "BackInsert() "<< chrono;
@@ -140,49 +90,13 @@ namespace vector_bench{
         cout << "Le vecteur contient " << vecteurTexte.size()<< " elements." << endl;
 
         // On trie par ordre alphébétique
-        size_t i=1;
-        do{
-            string temp;
-            size_t  j=i;
-
-            // On met en minuscule pour comparer
-            string moti = vecteurTexte[i], motjmoins1 = vecteurTexte[j-1];
-            transform(moti.begin(),moti.end(),moti.begin(),::tolower);
-            transform(motjmoins1.begin(),motjmoins1.end(),motjmoins1.begin(),::tolower);
-
-            while (j>0 && (moti < motjmoins1)) {
-                --j ; // On compare au moins précédent
-                if (j>0){
-                    // on met en minuscule le mot précédent
-                    motjmoins1 = vecteurTexte[j-1];
-                    transform(motjmoins1.begin(),motjmoins1.end(),motjmoins1.begin(),::tolower);
-                }
-            }
-
-            // Si j!=i c'est qu'on a trouvé une meilleurs place
-            if (j != i){
-                temp            = vecteurTexte[j];
-                vecteurTexte[j] = vecteurTexte[i];
-                vecteurTexte[i] = temp;
-            }else ++i; // Sinon on passe au mot suivant
-            
-        }while(i < vecteurTexte.size());
-
+        sort(vecteurTexte.begin(), vecteurTexte.end());
+       
         // On affiche tous les élements sur une ligne
         //for(string motCourant : vecteurTexte) cout << motCourant << " ";
 
         // On arrête le chrono
         chrono.stop();
         cout << "FrontInsert() "<< chrono;
-
-
-
     }
-
-
-
-
-
-
-
 }
